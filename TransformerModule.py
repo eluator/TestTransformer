ae474578516a830d06f9559c2536fe90b22605ed
eluator <tiv.eluator@gmail.com>
2024-08-15 16:27:40 +0200

    first training

---
+++ TransformerModule.py
@@ -0,0 +1,252 @@
+import einops
+from einops import einsum
+import math
+import torch
+from easy_transformer import EasyTransformer
+from torch import optim, nn, utils, Tensor
+from dataclasses import dataclass
+from easy_transformer.utils import get_corner, gelu_new, tokenize_and_concatenate
+
+reference_gpt2 = EasyTransformer.from_pretrained("gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False, device = 'cpu')
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text)
+logits, cache = reference_gpt2.run_with_cache(reference_text)
+
+
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    random_input = torch.randn(shape).cuda()
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    print("Output shape:", output.shape)
+    print()
+    return output
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    random_input = torch.randint(100, 1000, shape).cuda()
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    print("Output shape:", output.shape)
+    print()
+    return output
+
+def load_gpt2_test(cls, gpt2_layer, input_name, cache_dict=cache.cache_dict):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    # Allow inputs of strings or tensors
+    if isinstance(input_name, str): 
+        reference_input = cache_dict[input_name]
+    else:
+        reference_input = input_name
+    print("Input shape:", reference_input.shape)
+    output = layer(reference_input)
+    print("Output shape:", output.shape)
+    reference_output = gpt2_layer(reference_input)
+    print("Reference output shape:", reference_output.shape)
+
+    comparison = torch.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct")
+    return output
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(torch.ones(cfg.d_model))
+        self.b = nn.Parameter(torch.zeros(cfg.d_model))
+        self.test = 3
+        
+    def forward(self, residual):
+        # residual: [batch, position, d_model]
+        "YOUR CODE HERE"
+        if self.cfg.debug: print("Residual:", residual.shape)
+        residual = residual - einops.reduce(residual, "batch position d_model -> batch position 1", "mean")
+        # Calculate the variance, square root it. Add in an epsilon to prevent divide by zero.
+        scale = (einops.reduce(residual.pow(2), "batch position d_model -> batch position 1", "mean") + self.cfg.layer_norm_eps).sqrt()
+        normalized = residual / scale
+        normalized = normalized * self.w + self.b
+        if self.cfg.debug: print("Normalized:", residual.shape)
+        return normalized
+
+class Embed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(torch.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+    
+    def forward(self, tokens):
+        # tokens: [batch, position]
+        "YOUR CODE HERE"
+        if self.cfg.debug: print("Tokens:", tokens.shape)
+        embed = self.W_E[tokens, :] # [batch, position, d_model]
+        if self.cfg.debug: print("Embeddings:", embed.shape)
+        return embed
+
+class PosEmbed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(torch.empty((cfg.n_ctx, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.cfg.init_range)
+    
+    def forward(self, tokens):
+        # tokens: [batch, position]
+        if self.cfg.debug: print("Tokens:", tokens.shape)
+        pos_embed = self.W_pos[:tokens.size(1), :] # [position, d_model]
+        pos_embed = einops.repeat(pos_embed, "position d_model -> batch position d_model", batch=tokens.size(0))
+        if self.cfg.debug: print("pos_embed:", pos_embed.shape)
+        return pos_embed
+
+class Attention(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_Q, std=self.cfg.init_range)
+        self.b_Q = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.W_K = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_K, std=self.cfg.init_range)
+        self.b_K = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.W_V = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_V, std=self.cfg.init_range)
+        self.b_V = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        
+        self.W_O = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        nn.init.normal_(self.W_O, std=self.cfg.init_range)
+        self.b_O = nn.Parameter(torch.zeros((cfg.d_model)))
+        
+        self.register_buffer("IGNORE", torch.tensor(0.0, dtype=torch.float32, device="cuda"))
+    
+    def forward(self, normalized_resid_pre):
+        # normalized_resid_pre: [batch, position, d_model]
+        "YOUR CODE HERE"
+        q = einsum(self.W_Q, normalized_resid_pre, "n_heads d_model d_head, batch position d_model -> batch position n_heads d_head") + self.b_Q
+        k = einsum(self.W_K, normalized_resid_pre, "n_heads d_model d_head, batch position d_model -> batch position n_heads d_head") + self.b_K
+        v = einsum(self.W_V, normalized_resid_pre, "n_heads d_model d_head, batch position d_model -> batch position n_heads d_head") + self.b_V
+        qk = einsum(q, k, "batch query_pos n_heads d_head, batch key_pos n_heads d_head -> batch n_heads query_pos key_pos")
+        attn_scores = qk / math.sqrt(self.cfg.d_head)
+        attn_scores = self.apply_causal_mask(attn_scores)
+        pattern = attn_scores.softmax(dim=-1) # [batch, n_heads, query_pos, key_pos]
+        z = einsum(pattern, v, "batch n_heads query_pos key_pos, batch key_pos n_heads d_head -> batch query_pos n_heads d_head")
+        attn_out = einsum(z, self.W_O, "batch query_pos n_heads d_head, n_heads d_head d_model -> batch query_pos d_model") + self.b_O
+        return attn_out
+        
+    def apply_causal_mask(self, attn_scores):
+        # attn_scores: [batch, n_heads, query_pos, key_pos]
+        mask = torch.triu(torch.ones(attn_scores.size(-2), attn_scores.size(-1), device=attn_scores.device), diagonal=1).bool()
+        attn_scores.masked_fill_(mask, self.IGNORE)
+        return attn_scores
+
+class MLP(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(torch.empty((cfg.d_model, cfg.d_mlp)))
+        nn.init.normal_(self.W_in, std=self.cfg.init_range)
+        self.b_in = nn.Parameter(torch.zeros((cfg.d_mlp)))
+        self.W_out = nn.Parameter(torch.empty((cfg.d_mlp, cfg.d_model)))
+        nn.init.normal_(self.W_out, std=self.cfg.init_range)
+        self.b_out = nn.Parameter(torch.zeros((cfg.d_model)))
+    
+    def forward(self, normalized_resid_mid):
+        # normalized_resid_mid: [batch, position, d_model]
+        if self.cfg.debug: print("Normalized_resid_mid:", normalized_resid_mid.shape)
+        pre = einsum(normalized_resid_mid, self.W_in, "batch position d_model, d_model d_mlp -> batch position d_mlp") + self.b_in
+        post = gelu_new(pre)
+        mlp_out = einsum(post, self.W_out, "batch position d_mlp, d_mlp d_model -> batch position d_model") + self.b_out
+        return mlp_out
+
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(torch.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(torch.zeros((cfg.d_vocab), requires_grad=False))
+    
+    def forward(self, normalized_resid_final):
+        # normalized_resid_final [batch, position, d_model]
+        if self.cfg.debug: print("Normalized_resid_final:", normalized_resid_final.shape)
+        logits = einsum(normalized_resid_final, self.W_U, "batch position d_model, d_model d_vocab -> batch position d_vocab") + self.b_U
+        return logits
+
+class AttentionOnly(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.ln1 = LayerNorm(cfg)
+        self.attn = Attention(cfg)
+        self.ln2 = LayerNorm(cfg)
+    
+    def forward(self, resid_pre):
+        # resid_pre [batch, position, d_model]
+        normalized_resid_pre = self.ln1(resid_pre)
+        attn_out = self.attn(normalized_resid_pre)
+        resid_mid = resid_pre + attn_out
+        
+        normalized_resid_mid = self.ln2(resid_mid)
+        resid_post = resid_mid + normalized_resid_mid
+        return resid_post
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.ln1 = LayerNorm(cfg)
+        self.attn = Attention(cfg)
+        self.ln2 = LayerNorm(cfg)
+        self.mlp = MLP(cfg)
+    
+    def forward(self, resid_pre):
+        # resid_pre [batch, position, d_model]
+        normalized_resid_pre = self.ln1(resid_pre)
+        attn_out = self.attn(normalized_resid_pre)
+        resid_mid = resid_pre + attn_out
+        
+        normalized_resid_mid = self.ln2(resid_mid)
+        mlp_out = self.mlp(normalized_resid_mid)
+        resid_post = resid_mid + mlp_out
+        return resid_post
+
+class DemoTransformer(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.embed = Embed(cfg)
+        self.pos_embed = PosEmbed(cfg)
+        self.blocks = nn.ModuleList([TransformerBlock(cfg) for _ in range(cfg.n_layers)])
+        self.ln_final = LayerNorm(cfg)
+        self.unembed = Unembed(cfg)
+    
+    def forward(self, tokens):
+        # tokens [batch, position]
+        embed = self.embed(tokens)
+        pos_embed = self.pos_embed(tokens)
+        residual = embed + pos_embed
+        for block in self.blocks:
+            residual = block(residual)
+        normalized_resid_final = self.ln_final(residual)
+        logits = self.unembed(normalized_resid_final)
+        # logits have shape [batch, position, logits]
+        return logits
+
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
